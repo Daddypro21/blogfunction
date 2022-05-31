a63d09fd9df51d18ba1eb5cb756f5b7125e190b1
Daddypro21 <sndemapro@gmail.com>
2022-05-31 15:48:01 +0100

    modification du fichier contact.view

--- view/contact.view.php
+++ view/contact.view.php
@@ -1,14 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+<?php ob_start() ?>
     
 
-    
-</body>
-</html>
+    <h1>Je suis la page d'accueil de contact </h1>
+
+<?php 
+$content = ob_get_clean();
+require "./template/tmpl.php";
+?>
