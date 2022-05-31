d8aa4c21f517634754e517df1884e20dc7a0b658
Daddypro21 <sndemapro@gmail.com>
2022-05-31 15:50:02 +0100

    modification du fichier home.view.php

--- view/home.view.php
+++ view/home.view.php
@@ -1,15 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+<?php ob_start() ?>
     
 
     <h1>Je suis la page d'accueil du blog </h1>
 
-</body>
-</html>
+<?php 
+$content = ob_get_clean();
+require "../template/tmpl.php";
+?>
