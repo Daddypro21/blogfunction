ee0eb0607c65dbee5c6601001c4ddb8b58d76dd8
Daddypro21 <sndemapro@gmail.com>
2022-06-01 13:18:27 +0100

    ajout de commentaire

--- autoload/autoload.php
+++ autoload/autoload.php
@@ -16,6 +16,14 @@ function autoload()
         }
 
     }
+
+    /**
+     * Ce fichier a été déplacé dans un dossier different,raison pour laquelle
+     *elle n'est pas pris en charge dans la boucle foreach et n'est pas appélé
+     *dynamiquement...ce problème sera résolu,car nous continuons à travailler
+     *sur le projet
+     */
+    
     require "../functions/view/view.php";
 }
 autoload();
