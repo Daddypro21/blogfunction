bd37b583d737911f7feff3d9a7e7d0b15d203fb6
Daddypro21 <sndemapro@gmail.com>
2022-06-04 12:19:13 +0100

    Ajout de la vue createPost.view

---
+++ view/createPost.view.php
@@ -0,0 +1,40 @@
+<?php ob_start() ?>
+    
+<div class= "container"> 
+
+    <h1>Je suis la page qui crée un nouvel article </h1>
+
+    <form action="createPost/id" method ="POST">
+    <div class="form-group">
+        <label for="title" >Le châpo</label>
+        <input type="text" class="form-control" id="title" name="chapo" placeholder=
+        "Modifier le Chapo">
+    </div>
+    <br>
+    <div class="form-group">
+        <label for="title" >Titre de l'article</label>
+        <input type="text" class="form-control" id="title" name="title" placeholder=
+        "Modifier le titre">
+    </div>
+    <br>
+    <div class="form-group">
+        <input type="hidden" class="form-control" id="token" name="token" value="">  
+    </div>
+    <br>
+    <div class="form-group">
+        <label for="content" >Contenu de l'article</label>
+        <textarea name="content" id="content"  rows="8" class="form-control">
+        Le nouveau contenu
+        </textarea>
+    </div>
+    <br><br>
+    <button type="submit" class="btn btn-primary">Enregistrer les modifications</button>
+
+    <br><br>
+</form>
+</div>
+
+<?php 
+$content = ob_get_clean();
+require "../template/tmpl.php";
+?>
